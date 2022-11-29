14f7594f713c5779d33c53ece5d06d1df3b09903
Carl <tancj2785@gmail.com>
2022-11-29 17:11:30 +0800

    2 hours of work I also had to cheat sry

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string,times) {
+   if(times<0){
+    return 'ERROR';
+   }else if(times === 1){
+    return string;
+   }else if(times>0){
+    return string + repeatString(string,times-1);
+   }else{
+    return '';
+   }
 };
+repeatString('',Number);
 
 // Do not edit below this line
 module.exports = repeatString;
