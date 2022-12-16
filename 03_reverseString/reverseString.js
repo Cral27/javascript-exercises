7b723d978cec256c2a16022eff2d0636e07910a8
Carl <tancj2785@gmail.com>
2022-12-16 11:47:00 +0800

    after 2 weeks of hiatus finally did this one

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
 
+const reverseString = function(string) {
+    let wordSplit = string.split("");
+    let wordReverse = wordSplit.reverse("");
+    let wordJoin = wordReverse.join("");
+    return wordJoin;
 };
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;
