9ba446576c1fab5a29d534210ef00c160119d7c0
Carl <tancj2785@gmail.com>
2022-12-22 00:04:20 +0800

    finished within an hour this is my alley

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    switch (true){
+        case(year === 0):
+            return false;
+            break;
+        case(year%100 === 0 && year%400 === 0):
+            return true;
+            break;
+        case(year%100 === 0):
+            return false;
+            break;
+        case(year%4 === 0):
+            return true;
+            break;
+        case(year%4 === 1 || year%4 === 2 || year%4 === 3):
+        return false;
+        break;
+    }
 };
-
+leapYears(1900);
 // Do not edit below this line
 module.exports = leapYears;
